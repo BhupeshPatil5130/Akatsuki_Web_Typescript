bfa484b353ca4a844923e29fc31cf1598a487811
Bhupesh Patil <160297608+BhupeshPatil5130@users.noreply.github.com>
2024-09-18 16:37:33 +0530

    Remove unwanted comments

--- src/component/Alumni.tsx
+++ src/component/Alumni.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable react/no-unescaped-entities */
+
 import React from 'react';
 import { alumni2023 } from "../Data/alumniData";
 
--- src/component/Contact.tsx
+++ src/component/Contact.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/no-unescaped-entities */
+
 import React, { useState } from "react";
 
 interface FormData {
