8a09a22af96c82ec89da29ae327611f5095e4fca
Bhupesh Patil <160297608+BhupeshPatil5130@users.noreply.github.com>
2024-09-18 17:29:31 +0530

    Added TeamMember component in React App

---
+++ src/Data/teamMembers.ts
@@ -0,0 +1,126 @@
+// src/data/teamMembers.ts
+
+interface TeamMember {
+    name: string;
+    role: string;
+    image: string;
+    linkedin: string;
+    github: string;
+}
+
+const teamMembers: TeamMember[] = [
+    {
+        name: 'Mangesh Bide',
+        role: 'Ex. President',
+        image: '../images/Alumni/mangeshbide.jpg',
+        linkedin: 'https://www.linkedin.com/in/mangesh-bide/',
+        github: 'https://github.com/1Mangesh1'
+    },
+    {
+        name: 'Sakshi Kukreja',
+        role: 'Ex. Vice President',
+        image: '../images/Alumni/sakshi.jpg',
+        linkedin: 'https://www.linkedin.com/in/sakshi-kukreja-b67952217/',
+        github: ''
+    },
+    {
+        name: "Ravindra Pawar",
+        role: "MERN Stack Developer",
+        image: '../images/Alumni/ravi.jpg',
+        linkedin: "https://www.linkedin.com/in/ravindra55",
+        github: "https://github.com/ravi-pawar55"
+    },
+    {
+        name: "Durgesh Mahajan",
+        role: "Fullstack Developer",
+        image: '../images/Alumni/durgesh.jpg',
+        linkedin: "https://www.linkedin.com/in/prajakta-n-patil-11b18124a",
+        github: "https://github.com/Prajakta305"
+    },
+    {
+        name: "Vinay Salunkhe",
+        role: "Backend Developer",
+        image: '../images/Alumni/vinay.jpg',
+        linkedin: "https://www.linkedin.com/in/prajakta-n-patil-11b18124a",
+        github: "https://github.com/Prajakta305"
+    },
+    {
+        name: "Harsh Gawali",
+        role: "Fullstack Developer",
+        image: '../images/Alumni/harsh.jpg',
+        linkedin: "https://www.linkedin.com/in/prajakta-n-patil-11b18124a",
+        github: "https://github.com/Prajakta305"
+    },
+    {
+        name: "Suchita Patil",
+        role: "MERN Stack Developer",
+        image: '../images/Alumni/suchita.jpg',
+        linkedin: "https://www.linkedin.com/in/suchita-patil-29a86923a/",
+        github: "https://github.com/suchitapatil68"
+    },
+    {
+        name: "Samihan Nandedkar",
+        role: "Backend Developer",
+        image: '../images/Alumni/samihan.jpg',
+        linkedin: "https://www.linkedin.com/in/samihan15/",
+        github: "https://github.com/Samihan15"
+    },
+    {
+        name: "Rutuja Chavan",
+        role: "Backend Developer",
+        image: '../images/Alumni/rutuja.jpg',
+        linkedin: "https://www.linkedin.com/in/rutuja-chavan-837678236",
+        github: "https://github.com/chavanrutuja"
+    },
+    {
+        name: "Nikita Jangid",
+        role: "Frontend Developer",
+        image: '../images/Alumni/nikita.jpg',
+        linkedin: "https://www.linkedin.com/in/nikita-jangid-87b1b0246",
+        github: "https://github.com/nikita-2302"
+    },
+    {
+        name: "Prajakta Patil",
+        role: "Frontend Developer",
+        image: '../images/Alumni/prajakta.jpg',
+        linkedin: "https://www.linkedin.com/in/prajakta-n-patil-11b18124a",
+        github: "https://github.com/Prajakta305"
+    },
+    {
+        name: "Gitanjali Patil",
+        role: "Frontend Developer",
+        image: '../images/Alumni/gitanjali.jpeg',
+        linkedin: "https://www.linkedin.com/in/nikita-jangid-87b1b0246",
+        github: "https://github.com/nikita-2302"
+    },
+    {
+        name: "Hitesh Wadile",
+        role: "MERN Stack Developer",
+        image: '../images/members/hitesh.jpg',
+        linkedin: "https://www.linkedin.com/in/hitesh-wadile-7005b4221?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        github: "https://github.com/hitesh-wadile"
+    },
+    {
+        name: "Pruthviraj Chaudhari",
+        role: "MERN Stack Developer",
+        image: '../images/members/Pruthviraj.png',
+        linkedin: "https://www.linkedin.com/in/pruthviraj-chaudhari-8386ab230",
+        github: "https://github.com/Pruthviraj-chaudhari"
+    },
+    {
+        name: "Abhishek Pawar",
+        role: "MERN Stack Developer",
+        image: '../images/members/abhi.jpg',
+        linkedin: "https://www.linkedin.com/in/abhishek-pawar-837064241?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        github: "https://github.com/Abhishekpawar259"
+    },
+    {
+        name: "Saurav Patil",
+        role: "React Developer",
+        image: '../images/members/saurav.jpg',
+        linkedin: "https://www.linkedin.com/in/saurav-patil-199505247/",
+        github: "https://github.com/Scammerpatil"
+    },
+];
+
+export default teamMembers;
